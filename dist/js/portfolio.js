01ea89e5dec21aca7d9782b863af1836195cf759
playua18 <playua18@gmail.com>
2017-09-13 17:29:01 +0300

    add works.html

---
+++ dist/js/portfolio.js
@@ -0,0 +1,6 @@
+var portfBtn = document.querySelector('.portfolio__to-all');
+
+portfBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+    portfBtn.classList.toggle('portfolio__to-all--show');
+});
