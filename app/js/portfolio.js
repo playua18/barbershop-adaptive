a415164242062809ddc5e6b33967b8b73d63e4e9
playua18 <playua18@gmail.net>
2017-11-20 01:46:33 +0200

    del app

--- app/js/portfolio.js
+++
@@ -1,5 +0,0 @@
-var portfBtn = document.querySelector('.portfolio__to-all');
-portfBtn.addEventListener('click', function (event) {
-  event.preventDefault();
-    portfBtn.classList.toggle('portfolio__to-all--show');
-});
